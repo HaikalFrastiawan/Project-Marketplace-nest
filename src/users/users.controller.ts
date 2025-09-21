29111c94062d7ef6662abd77738ed2a8c232a40e
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-21 21:39:04 +0700

    1.4 method create user dan validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body ,Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-users.dto';
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Post()
+    createUser(@Body() body: CreateUserDto) {
+        return body;
+    }
+
+
+}
+
+
